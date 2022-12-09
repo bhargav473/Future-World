817e3a5846fc934cd2d43ac65ea2a96c0d118b00
FIRST_NAME LAST_NAMEgit config --global user.name <bhargav.k@fwc.co.in>
2022-12-09 15:15:44 +0530

    form update

--- assets/js/master.js
+++ assets/js/master.js
@@ -414,34 +414,34 @@
 
   /* ===== CONTACT FORM ===== */
 
-	$(function () {
+//	$(function () {
+//
+//    $('#contact-form').validator();
 
-    $('#contact-form').validator();
-
-    $('#contact-form').on('submit', function (e) {
-
-        if (!e.isDefaultPrevented()) {
-            var url = "assets/php/contact.php";
-            $.ajax({
-                type: "POST",
-                url: url,
-                data: $(this).serialize(),
-                success: function (data)
-                {
-                    var messageAlert = data.class;
-                    var messageText = data.message;
-
-                    var alertBox = '<div class="' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
-                    if (messageAlert && messageText) {
-                        $('#contact-form').find('.messages').html(alertBox);
-                        $('#contact-form')[0].reset();
-                    }
-                }
-            });
-            return false;
-        }
-    	});
-	});
+//    $('#contact-form').on('submit', function (e) {
+//
+//        if (!e.isDefaultPrevented()) {
+//            var url = "assets/php/contact.php";
+//            $.ajax({
+//                type: "POST",
+//                url: url,
+//                data: $(this).serialize(),
+//                success: function (data)
+//                {
+//                    var messageAlert = data.class;
+//                    var messageText = data.message;
+//
+//                    var alertBox = '<div class="' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+//                    if (messageAlert && messageText) {
+//                        $('#contact-form').find('.messages').html(alertBox);
+//                        $('#contact-form')[0].reset();
+//                    }
+//                }
+//            });
+//            return false;
+//        }
+//    	});
+//	});
 
 
 	/* ===== REVOLUTION SLIDER MAIN ===== */
